6ac32c3d17210ba97efaaa078c59567b4430e4a9
Joel Hofgren <joel.hofgren@squeed.com>
2019-09-20 19:14:51 +0200

    Replace Conditional with Polymorphism, step 3.
    Delete old getUrl method.

--- api-caller/src/main/java/com/squeed/codefoundation/cleancode/service/MovieService.java
+++ api-caller/src/main/java/com/squeed/codefoundation/cleancode/service/MovieService.java
@@ -11,9 +11,6 @@ import java.util.List;
 @Service
 public class MovieService {
 
-    public static final String OMDB_HOST_URL = "http://www.omdbapi.com/";
-    public static final String APIKEY = "apikey=c626976c";
-
     @Autowired
     private RestTemplate restTemplate;
 
@@ -48,7 +45,9 @@ public class MovieService {
             return null;
         }
 
-        String url = getUrl(omdbApiParameters);
+        OmdbApi api = new OmdbApi(omdbApiParameters);
+
+        String url = api.getUrl();
 
         List<Movie> movies = fetchMovies(url, omdbApiParameters);
 
@@ -71,67 +70,6 @@ public class MovieService {
             }
         }
         return movies;
-    }
-
-    private String getUrl(OmdbApiParameters omdbApiParameters) {
-        String url = OMDB_HOST_URL + "?" + APIKEY;
-
-        if (omdbApiParameters.getSearch() != null) {
-            url += "&s=" + omdbApiParameters.getSearch();
-            if (omdbApiParameters.getType() != null) {
-                if (omdbApiParameters.getType().equalsIgnoreCase("movie")) {
-                    url += "&type=movie";
-                } else if (omdbApiParameters.getType().equalsIgnoreCase("series")) {
-                    url += "&type=series";
-                } else if (omdbApiParameters.getType().equalsIgnoreCase("episode")) {
-                    url += "&type=episode";
-                }
-            }
-
-            if (omdbApiParameters.getYear() > 0) {
-                url += "&y=" + new Integer(omdbApiParameters.getYear()).toString();
-            }
-
-            if (omdbApiParameters.getReturnType() != null) {
-                url += "&r=" + omdbApiParameters.getReturnType();
-            }
-
-            if (omdbApiParameters.getPage() > 0 && omdbApiParameters.getPage() <= 100) {
-                url += "&page=" + omdbApiParameters.getPage();
-            }
-        }
-
-        if (omdbApiParameters.getImdbId() != null) {
-            url += "&i=" + omdbApiParameters.getImdbId();
-        } else if (omdbApiParameters.getTitle() != null) {
-            url += "&t=" + omdbApiParameters.getTitle();
-        }
-
-        if (omdbApiParameters.getType() != null) {
-            if (omdbApiParameters.getType().equalsIgnoreCase("movie")) {
-                url += "&type=movie";
-            } else if (omdbApiParameters.getType().equalsIgnoreCase("series")) {
-                url += "&type=series";
-            } else if (omdbApiParameters.getType().equalsIgnoreCase("episode")) {
-                url += "&type=episode";
-            }
-        }
-
-        if (omdbApiParameters.getYear() > 0) {
-            url += "&y=" + new Integer(omdbApiParameters.getYear()).toString();
-        }
-
-        if (omdbApiParameters.getPlot() == 0) {
-            url += "&plot=short";
-        } else {
-            url += "&plot=full";
-        }
-
-        if (omdbApiParameters.getReturnType() != null) {
-            url += "&r=" + omdbApiParameters.getReturnType();
-        }
-
-        return url;
     }
 
     private static boolean notEnoughParameters(String search, String imdbId, String title) {
