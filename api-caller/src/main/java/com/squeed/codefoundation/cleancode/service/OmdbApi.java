927e81a7bec0bd3c8d999ab6242026e601a235c7
Joel Hofgren <joel.hofgren@squeed.com>
2019-09-20 19:07:56 +0200

    Replace Conditional with Polymorphism, step 1.
    Create target class.

---
+++ api-caller/src/main/java/com/squeed/codefoundation/cleancode/service/OmdbApi.java
@@ -0,0 +1,10 @@
+package com.squeed.codefoundation.cleancode.service;
+
+public class OmdbApi {
+
+    private final OmdbApiParameters omdbApiParameters;
+
+    public OmdbApi(OmdbApiParameters omdbApiParameters) {
+        this.omdbApiParameters = omdbApiParameters;
+    }
+}
