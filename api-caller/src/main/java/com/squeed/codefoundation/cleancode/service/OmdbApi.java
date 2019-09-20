08e2735fc326b1fc337d8bd357e4c08d0159ee4e
Joel Hofgren <joel.hofgren@squeed.com>
2019-09-20 19:12:07 +0200

    Replace Conditional with Polymorphism, step 2.
    Move getUrl method to target class.

--- api-caller/src/main/java/com/squeed/codefoundation/cleancode/service/OmdbApi.java
+++ api-caller/src/main/java/com/squeed/codefoundation/cleancode/service/OmdbApi.java
@@ -2,9 +2,74 @@ package com.squeed.codefoundation.cleancode.service;
 
 public class OmdbApi {
 
+    public static final String OMDB_HOST_URL = "http://www.omdbapi.com/";
+    public static final String APIKEY = "apikey=c626976c";
+
     private final OmdbApiParameters omdbApiParameters;
 
     public OmdbApi(OmdbApiParameters omdbApiParameters) {
         this.omdbApiParameters = omdbApiParameters;
+    }
+
+    public String getUrl() {
+        String url = OMDB_HOST_URL + "?" + APIKEY;
+
+        if (omdbApiParameters.getSearch() != null) {
+            url += "&s=" + omdbApiParameters.getSearch();
+            if (omdbApiParameters.getType() != null) {
+                if (omdbApiParameters.getType().equalsIgnoreCase("movie")) {
+                    url += "&type=movie";
+                } else if (omdbApiParameters.getType().equalsIgnoreCase("series")) {
+                    url += "&type=series";
+                } else if (omdbApiParameters.getType().equalsIgnoreCase("episode")) {
+                    url += "&type=episode";
+                }
+            }
+
+            if (omdbApiParameters.getYear() > 0) {
+                url += "&y=" + new Integer(omdbApiParameters.getYear()).toString();
+            }
+
+            if (omdbApiParameters.getReturnType() != null) {
+                url += "&r=" + omdbApiParameters.getReturnType();
+            }
+
+            if (omdbApiParameters.getPage() > 0 && omdbApiParameters.getPage() <= 100) {
+                url += "&page=" + omdbApiParameters.getPage();
+            }
+        }
+
+        if (omdbApiParameters.getImdbId() != null) {
+            url += "&i=" + omdbApiParameters.getImdbId();
+        } else if (omdbApiParameters.getTitle() != null) {
+            url += "&t=" + omdbApiParameters.getTitle();
+        }
+
+        if (omdbApiParameters.getType() != null) {
+            if (omdbApiParameters.getType().equalsIgnoreCase("movie")) {
+                url += "&type=movie";
+            } else if (omdbApiParameters.getType().equalsIgnoreCase("series")) {
+                url += "&type=series";
+            } else if (omdbApiParameters.getType().equalsIgnoreCase("episode")) {
+                url += "&type=episode";
+            }
+        }
+
+        if (omdbApiParameters.getYear() > 0) {
+            url += "&y=" + new Integer(omdbApiParameters.getYear()).toString();
+        }
+
+        if (omdbApiParameters.getPlot() == 0) {
+            url += "&plot=short";
+        } else {
+            url += "&plot=full";
+        }
+
+        if (omdbApiParameters.getReturnType() != null) {
+            url += "&r=" + omdbApiParameters.getReturnType();
+        }
+
+        return url;
+
     }
 }
